b5d452d5255ed18308dde80e32ff24b4e30b8b07
PRIYANKA SHANKARAN <priyan@ncsu.edu>
2015-03-22 23:18:50 -0400

    first commit

--- main.js
+++ main.js
@@ -3,22 +3,53 @@ var multer  = require('multer')
 var express = require('express')
 var fs      = require('fs')
 var app = express()
+var client = redis.createClient(6379, '127.0.0.1', {})
+//var value=9;
+var a;
+var mylist= [];
+var htmlpage=[];
+//client.set("a", "this message will self-destruct in 10 seconds");
+
+//client.get("a", function(err,value){console.log(value);a=value;});
+
+//client.expire("a", 10);
+
+//client.get("a", function(err,value){a=value;});
 // REDIS
 //var client = redis.createClient(6379, '127.0.0.1', {})
 
 ///////////// WEB ROUTES
 
 // Add hook to make it easier to get all visited URLS.
+
+
+
+
 app.use(function(req, res, next) 
 {
 	console.log(req.method, req.url);
 
 	// ... INSERT HERE.
+	client.lpush(mylist, req.url, function(err,value){console.log(value)});
+	//console.log(mylist);
+	//client.ltrim(mylist,0,4, );
+	client.lrange(mylist,0,4, function(err,value){htmlpage=value});
+
 
 	next(); // Passing the request to the next handler in the stack.
 });
 
+app.get('/recent', function(req, res) {
+  res.send(htmlpage)
+})
 
+app.get('/get', function(req, res) {
+  res.send(value)
+})
+
+app.get('/', function(req, res) {
+  res.send('hello world')
+})
 // app.post('/upload',[ multer({ dest: './uploads/'}), function(req, res){
 //    console.log(req.body) // form fields
 //    console.log(req.files) // form files
@@ -48,11 +79,13 @@ app.use(function(req, res, next)
 // })
 
 // HTTP SERVER
-// var server = app.listen(3000, function () {
+ var server = app.listen(3000, function () {
 
-//   var host = server.address().address
-//   var port = server.address().port
+   var host = server.address().address
+   var port = server.address().port
 
-//   console.log('Example app listening at http://%s:%s', host, port)
-// })
+   console.log('Example app listening at http://%s:%s', host, port)
+ })
+
+ 
 
